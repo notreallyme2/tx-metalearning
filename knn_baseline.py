03a6150ac266e6d35ca54ebbbf8cda199c195057
Ubuntu <ubuntu@ip-172-31-0-105.eu-west-1.compute.internal>
2020-05-07 12:09:36 +0000

    Working KNN CLI and KNN results

---
+++ knn_baseline.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # KNN benchmark
+
+import os
+from pathlib import Path
+from time import time
+from tqdm import tqdm
+
+from fire import Fire
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn as skl
+from sklearn.model_selection import KFold, ParameterGrid
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.metrics import roc_auc_score
+
+from resampling import NestedCV
+from utilities import hdf_keys
+
+DEFAULT_DATASET_PATH = Path("/data/pfizer_tx/tasks_all_clr/all_clr_train_LUAD_stage.h5")
+DEFAULT_HPARAMS = dict()
+DEFAULT_HPARAMS['k'] = [1, 3, 5, 7, 9]
+
+def cross_val_loop(n_splits_outer=5, n_splits_inner=5, dataset_path=DEFAULT_DATASET_PATH, hparams=DEFAULT_HPARAMS):
+    """Sends a single data set (stored as an h5 file by Pandas) to a nested CV loop.  
+    A KNN model is trained on multiple hyperparameters in the inner loop.  
+    Unbiased performance is assessed in the outer loop.  
+    Output is saved to a file named "<dataset_path>_knn.csv" in the local folder ./results/  
+    
+    Parameters
+    ----------
+    n_splits_outer : int
+    n_splits_inner : int
+    dataset_path : str or Path
+    hparams : dict
+    
+    """
+    if not os.path.exists("./results/"):
+        os.mkdir("./results/")
+    results = []
+    best_params = []
+    results_path = Path(f"./results/{dataset_path.stem}_knn.csv")
+    print(f"Training on {dataset_path}")
+    keys = hdf_keys(dataset_path)
+    test_data = {key : pd.read_hdf(dataset_path, key = key) for key in keys}
+    def knn_model(params):
+        return KNN(n_neighbors=params['k'], n_jobs=-1)
+    nestedCV = NestedCV(knn_model, hparams, n_splits_outer, n_splits_inner)
+    performance, params = nestedCV.train(test_data['/expression'], test_data['/labels'])
+    results = pd.DataFrame([performance, params]).transpose()
+    results.columns = ["auc", "params"]
+    results.to_csv(results_path, index=False)
+    
+if __name__ == "__main__":
+    Fire(cross_val_loop)
+os._exit(0)
+
+
+
+
+
--- resampling.py
+++ resampling.py
@@ -12,7 +12,7 @@ from sklearn.metrics import roc_auc_score
 
 
 class NestedCV:
-    def __init__(self, model, hparams, n_splits_outer=5):
+    def __init__(self, model, hparams, n_splits_outer=5, n_splits_inner=5):
         """Nested cross validation class. 
         Currently only uses AUROC as its performance metric.
         
@@ -29,31 +29,33 @@ class NestedCV:
         """
         self.model = model
         self.hparams = hparams
-        self.n_splits_outer=5
-        self.n_splits_inner = np.prod([len(hparams[k]) for k in hparams])
-        print(f"Number of inner splits (product of all hparam values): {self.n_splits_inner}")
+        self.n_splits_outer = n_splits_outer
+        self.n_splits_inner = n_splits_inner
         
     def _inner_loop(self, input_x, input_y):
         inner = KFold(n_splits=self.n_splits_inner)
         params = ParameterGrid(self.hparams)
-        best_k = None
-        best_auc = 0.5
-        for idx, (t, v) in enumerate(inner.split(input_x)):
-            this_model = self.model(params[idx])
-            x_train, y_train = input_x.iloc[t], input_y.iloc[t]
-            x_valid, y_valid = input_x.iloc[v], input_y.iloc[v]
-            print(f"Fitting model with params: {params[idx]}")
-            fitted_model = this_model.fit(x_train, y_train)
-            valid_prob = fitted_model.predict_proba(x_valid)
-            valid_auc = roc_auc_score(y_true=y_valid, y_score=valid_prob[:,1])
-            if valid_auc > best_auc:
-                best_auc = valid_auc
-                best_params = params[idx]
-            # finally train on all the data and predict held out sample
+        auc_grid = np.zeros([self.n_splits_inner, len(params)]) # holds all the performance metrics for each k and each fold
+        for split_idx, (t, v) in enumerate(inner.split(input_x)):
+            print(f"Inner fold {split_idx+1} of {self.n_splits_inner}")
+            for param_idx, param in enumerate(params):
+                this_model = self.model(param)
+                x_train, y_train = input_x.iloc[t], input_y.iloc[t]
+                x_valid, y_valid = input_x.iloc[v], input_y.iloc[v]
+                print(f"Fitting model with params: {param}")
+                fitted_model = this_model.fit(x_train, y_train)
+                valid_prob = fitted_model.predict_proba(x_valid)
+                valid_auc = roc_auc_score(y_true=y_valid, y_score=valid_prob[:,1])
+                auc_grid[split_idx, param_idx] = valid_auc
+        # finally get best param, train on all the data and return a trained model
+        mean_auc = np.mean(auc_grid, axis=0)
+        best_idx = np.argmax(mean_auc)
+        best_auc = mean_auc[best_idx]
+        best_params = params[best_idx]
         print(f"Best params: {best_params}, training final model")
         this_model = self.model(best_params)
         fitted_model = this_model.fit(input_x, input_y)
-        return fitted_model
+        return fitted_model, best_params
     
     def _outer_loop(self, X, Y):
         start = time()
@@ -61,13 +63,13 @@ class NestedCV:
         performance = []
         best_params = []
         for idx, (t, v) in enumerate(outer.split(X)):
-            print(f"Fold {idx+1}")
+            print(f"Outer fold {idx+1} of {self.n_splits_outer}")
             x_train, y_train = X.iloc[t], Y.iloc[t]
             x_test, y_test = X.iloc[v], Y.iloc[v]
-            this_model_outer = self._inner_loop(x_train, y_train)
+            this_model_outer, this_model_params = self._inner_loop(x_train, y_train)
             y_test_prob = this_model_outer.predict_proba(x_test)
             performance.append(roc_auc_score(y_true=y_test, y_score=y_test_prob[:,1]))
-            best_params.append(this_model_outer.get_params())
+            best_params.append(this_model_params)
         time_taken = time() - start
         return(performance, best_params, time_taken)
     
